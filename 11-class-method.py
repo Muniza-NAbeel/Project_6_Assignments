1034a170d036bb049ff4adb099405852f45b83f1
Muniza .. <159540004+Muniza-NAbeel@users.noreply.github.com>
2025-05-20 12:13:11 +0500

    11-class-method.py

--- 11-class-method.py
+++ 11-class-method.py
@@ -1,20 +1,14 @@
 class Book:
     total_books = 0  # Class variable to keep track of total books
 
-    def __init__(self, title):    # Constructor jo book ka title set karega
-        self.title = title
-        Book.Increment_total_books()   # Class method ko call karega jab bhi ek naya book object create hoga
-
+    def __init__(self):    # Constructor jo book ka title set karega
+        Book.increment_total_books()   # Class method ko call karega jab bhi ek naya book object create hoga
 
     @classmethod
     def increment_total_books(cls):    # Class method jo total books ko increment karega
         cls.total_books += 1
 
-    @classmethod
-    def display_count(cls):     # Class method jo total books ko display karega
-        print(f"Total books: {cls.total_books}")
-
-book1 = Book("Python")
-book2 = Book("Generative AI")
+book1 = Book()
+book2 = Book()
 
 Book.display_count()  
